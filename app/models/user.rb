4c29fa3c0d61abd84b40e02ee80feda78335bd5c
Brian Ethier <davidbrianethier@gmail.com>
2016-02-17 15:44:49 -0500

    Adding cart creation on User creation to make sure User has a cart

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_one :cart, dependent: :destroy
+
+  after_create :create_cart
+
+  private
+
+  def create_cart
+    Cart.find_or_create_by(:user => self)
+  end
 end
