7c590f7ffeae9f7ec479893a71d985139c63c319
Brian Ethier <davidbrianethier@gmail.com>
2016-02-17 20:34:38 -0500

    Adding in basic home page which shows the current sale and its items and variants/item_options

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,4 +1,9 @@
 class RootController < ApplicationController
   def index
+    @sale = Sale.includes(:items, {
+      sale_variants: {
+        variant: :item_options
+      }
+    }).first
   end
 end
--- app/models/variant.rb
+++ app/models/variant.rb
@@ -1,4 +1,6 @@
 class Variant < ApplicationRecord
+  belongs_to :item
+
   has_many :sales, through: :sale_variants
   has_many :sale_variants
 
