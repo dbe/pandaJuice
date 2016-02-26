8c11a2e5e9fa912040632242dcd1be29b9d1dd02
Brian Ethier <davidbrianethier@gmail.com>
2016-02-26 17:31:28 -0500

    Adding some more styling and layout for homepage to buy items

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ApplicationRecord
-  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :photo, styles: { medium: "300", small: "200", thumb: "100" }
   validates_attachment :photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
   belongs_to :item
